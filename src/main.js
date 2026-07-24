a05f682a59ed90d15f518d53066ed6321c049d8c
PaulKurdo <72892494+PaulKurdo@users.noreply.github.com>
2026-07-24 13:00:55 +0100

    Name update

--- src/main.js
+++ src/main.js
@@ -4,15 +4,15 @@ new App({
   target: document.body,
   props: {
     // What's your name?
-    name: "James Butler",
+    name: "Paul Kurdo",
     // In the following fiels you can either give a single string,
     // or an array of bullet points
 
     // What do you associate with the term 'CI/CD'?
-    associations: ["Ehm, what?"],
+    associations: ["continuous integration and continuous delivery"], ["gitlab CI/CD"]
     // Which CI/CD tools do you use in your project?
-    tools: "Jenkins, maybe?",
+    tools: "None",
     // What do you want to learn in this workshop?
-    expectations: ["what you are talking about"],
+    expectations: ["As much as I can about CI/CD, especially stuff I didn't know"],
   },
 });
